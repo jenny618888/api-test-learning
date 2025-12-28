83a2354c1c3ddbd64cb9fc52b6f7a0f645b7150b
jenny <hygge0618@gmail.com>
2025-12-28 13:06:04 +0800

    day1 python basics

---
+++ 2.10 字符串格式化.py
@@ -0,0 +1,12 @@
+name = "apple"
+message = " I want eat an %s" % name
+print(message)
+print(type(name))
+print(type(message))
+
+#数字类型占位
+class_num = 100
+avg_salary = 20
+message = "summary is %s, avarage salary is %s" % (class_num, avg_salary)
+print(message)
+
---
+++ 比较运算符.py
@@ -0,0 +1,5 @@
+num1 = 10
+num2 = 20
+print(f"10 != 20's result is {num1 != num2}")
+print(f"10 == 20's result is {num1 == num2}")
+print(f"10 > 20's result is {num1 > num2}")
