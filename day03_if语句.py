9e791624867e07ea1ebe9ca4cb841b6ba5508e65
jenny <hygge0618@gmail.com>
2025-12-31 21:46:55 +0800

    update

--- day03_if语句.py
+++ day03_if语句.py
@@ -1,4 +1,4 @@
-"""#input() get information from the keyboard
+#input() get information from the keyboard
 print("tell me who are you")
 name = input()
 print("I see, you are: %s" % name)
@@ -10,29 +10,29 @@ age = 30
 if age >= 18:
     print(" I am an adult")
     print(" I am going to the college")
-print("time is fast")"""
+print("time is fast")
 
-# print("Welcome to the park")
-# age = input("please enter your age")
-# if int(age) > 18:
-#     print("please afford more 10 dollars")
-# print("Hope you have fun")
-#
-# age = int(input("please enter your age"))
-# if age >= 18:
-#     print("please afford more 12 dollars")
-# else: #不需要判断条件
-#     print("Having fun")
+print("Welcome to the park")
+age = input("please enter your age")
+if int(age) > 18:
+    print("please afford more 10 dollars")
+print("Hope you have fun")
+
+age = int(input("please enter your age"))
+if age >= 18:
+    print("please afford more 12 dollars")
+else: #不需要判断条件
+    print("Having fun")
 
 
-# if int(input("please enter your height")) < 120:
-#     print("height less than 120, for free")
-# elif int(input("please enter your vip level")) > 3:
-#     print("vip level greater than 3, for free")
-# elif int(input("please tell me what day is it")) == 1:
-#     print("today is 1st, for free")
-# else:
-#     print("you need to buy the ticket")
+if int(input("please enter your height")) < 120:
+    print("height less than 120, for free")
+elif int(input("please enter your vip level")) > 3:
+    print("vip level greater than 3, for free")
+elif int(input("please tell me what day is it")) == 1:
+    print("today is 1st, for free")
+else:
+    print("you need to buy the ticket")
 
 num = 128
 if int(input("enter the first number you guess")) == num:
